9c67eed98a78c87ab0efb2f50a4cf90dac4d4681
sereaudrey <60405779+sereaudrey@users.noreply.github.com>
2021-06-21 13:23:37 +0200

    Add files via upload

---
+++ Jeu Chiffre/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Init_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CmdFermer = new System.Windows.Forms.Button();
+            this.LblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // CmdFermer
+            // 
+            this.CmdFermer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CmdFermer.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmdFermer.ForeColor = System.Drawing.Color.OliveDrab;
+            this.CmdFermer.Location = new System.Drawing.Point(323, 351);
+            this.CmdFermer.Name = "CmdFermer";
+            this.CmdFermer.Size = new System.Drawing.Size(139, 41);
+            this.CmdFermer.TabIndex = 0;
+            this.CmdFermer.Text = "Fermer la fenêtre";
+            this.CmdFermer.UseVisualStyleBackColor = false;
+            this.CmdFermer.Click += new System.EventHandler(this.CmdFermer_Click);
+            // 
+            // LblTime
+            // 
+            this.LblTime.Font = new System.Drawing.Font("Segoe Print", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTime.Location = new System.Drawing.Point(74, 92);
+            this.LblTime.Name = "LblTime";
+            this.LblTime.Size = new System.Drawing.Size(627, 102);
+            this.LblTime.TabIndex = 1;
+            this.LblTime.Text = "label1";
+            this.LblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblTime);
+            this.Controls.Add(this.CmdFermer);
+            this.Name = "Form1";
+            this.Text = "Mon Horloge";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdFermer;
+        private System.Windows.Forms.Label LblTime;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Jeu Chiffre/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Init_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public void Horloge()
+        {
+            //fonction de mise à l'heure de l'horloge
+            string MonHeure = DateTime.Now.ToString("HH:mm:ss");
+            LblTime.Text = MonHeure;
+            //timer1.Enabled = true;
+            return;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdFermer_Click(object sender, EventArgs e)
+        {
+            Form.ActiveForm.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Horloge();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Horloge();
+        }
+    }
+}
